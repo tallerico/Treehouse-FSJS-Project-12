47afc723438010f390f80d3944a89ba4dd68e95f
Mike Tallerico <mtallerico1@gmail.com>
2019-02-18 13:38:15 -0500

    sets up colors for theme with material-ui component library

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,22 @@
 import React, { Component } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles'
+import blue from '@material-ui/core/colors/blue'
+import SimpleMenu from './components/menu'
+import AppBar from './components/appBar'
+
+const theme = createMuiTheme({
+	palette: {
+		primary: { main: blue[500] }, // Purple and green play nicely together.
+		secondary: { main: blue[300] }, // This is just green.A700 as hex.
+	},
+})
 
 class App extends Component {
 	render() {
 		return (
-			<div className="App">
-				<header className="App-header">
-					<img src={logo} className="App-logo" alt="logo" />
-					<p>
-						Edit <code>src/App.js</code> and save to reload.
-					</p>
-					<a
-						className="App-link"
-						href="https://reactjs.org"
-						target="_blank"
-						rel="noopener noreferrer"
-					>
-						Learn React
-					</a>
-				</header>
-			</div>
+			<MuiThemeProvider theme={theme}>
+				<AppBar />
+			</MuiThemeProvider>
 		)
 	}
 }
