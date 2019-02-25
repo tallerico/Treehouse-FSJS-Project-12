50f5755157bf83174d444d4241799613d95cdc4d
Mike Tallerico <mtallerico1@gmail.com>
2019-02-25 18:12:55 -0500

    creates welcome component that displays welcome depending on user auth and time of day

--- client/src/components/welcome.js
+++ client/src/components/welcome.js
@@ -1,2 +1,48 @@
-import React, { Component } from 'react'
+import React from 'react'
+import Typography from '@material-ui/core/Typography'
 import '../App.css'
+
+function Welcome(props) {
+	const d = new Date()
+	const currHour = d.getHours()
+
+	if (props.isAuthenticated && currHour >= 0 && currHour < 12) {
+		return (
+			<div className="container_welcome">
+				<Typography variant="h4" color="inherit">
+					Good Morning, {props.firstName}!
+				</Typography>
+			</div>
+		)
+	}
+
+	if (props.isAuthenticated && currHour >= 12 && currHour < 17) {
+		return (
+			<div className="container_welcome">
+				<Typography variant="h4" color="inherit">
+					Good Afternoon, {props.firstName}!
+				</Typography>
+			</div>
+		)
+	}
+
+	if (props.isAuthenticated && currHour >= 17 && currHour <= 24) {
+		return (
+			<div className="container_welcome">
+				<Typography variant="h4" color="inherit">
+					Good Evening, {props.firstName}!
+				</Typography>
+			</div>
+		)
+	}
+
+	return (
+		<div className="container_welcome">
+			<Typography variant="h4" color="inherit">
+				Hello!
+			</Typography>
+		</div>
+	)
+}
+
+export default Welcome
