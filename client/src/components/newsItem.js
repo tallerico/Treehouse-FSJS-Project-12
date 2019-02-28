ac550c5ed1ab9b2b92d8eb03a8309d7e7c5bed81
Mike Tallerico <mtallerico1@gmail.com>
2019-02-27 19:51:44 -0500

    creates cards for individual news items

--- client/src/components/newsItem.js
+++ client/src/components/newsItem.js
@@ -1,11 +1,63 @@
 import React from 'react'
+import PropTypes from 'prop-types'
+import Link from '@material-ui/core/Link'
+import { withStyles } from '@material-ui/core/styles'
+import Card from '@material-ui/core/Card'
+import CardActionArea from '@material-ui/core/CardActionArea'
+import CardActions from '@material-ui/core/CardActions'
+import CardContent from '@material-ui/core/CardContent'
+import CardMedia from '@material-ui/core/CardMedia'
+import Button from '@material-ui/core/Button'
+import Typography from '@material-ui/core/Typography'
+import Fab from '@material-ui/core/Fab'
+import AddIcon from '@material-ui/icons/Add'
 
-function NewsItem(props) {
+const styles = theme => ({
+	card: {
+		maxWidth: 345,
+		marginBottom: 12,
+	},
+	media: {
+		height: 140,
+	},
+	link: {
+		margin: theme.spacing.unit,
+	},
+	fab: {
+		margin: theme.spacing.unit,
+	},
+})
+
+function MediaCard(props) {
+	const news = props.news
+	const { classes } = props
+
 	return (
-		<div>
-			<img src={props.image} alt="article" />
-		</div>
+		<Card className={classes.card}>
+			<CardActionArea>
+				<CardMedia className={classes.media} image={news.urlToImage} title={news.title} />
+				<CardContent>
+					<Typography gutterBottom variant="h6" component="h2">
+						{news.title}
+					</Typography>
+				</CardContent>
+			</CardActionArea>
+			<CardActions>
+				<Typography>
+					<Link href={news.url} className={classes.link}>
+						Read
+					</Link>
+				</Typography>
+				<Fab color="primary" aria-label="Add" className={classes.fab}>
+					<AddIcon />
+				</Fab>
+			</CardActions>
+		</Card>
 	)
 }
 
-export default NewsItem
+MediaCard.propTypes = {
+	classes: PropTypes.object.isRequired,
+}
+
+export default withStyles(styles)(MediaCard)
