b8d3e0de64f7d4ec3a0561a5c94d66cb07b5a410
Mike Tallerico <mtallerico1@gmail.com>
2019-02-27 19:51:04 -0500

    adds map function to create news cards based on amount of news stories

--- client/src/components/latestNews.js
+++ client/src/components/latestNews.js
@@ -1,16 +1,42 @@
 import React from 'react'
+import PropTypes from 'prop-types'
+import { withStyles } from '@material-ui/core/styles'
+import Grid from '@material-ui/core/Grid'
+import Paper from '@material-ui/core/Paper'
 import NewsItem from './newsItem'
 
-const LatestNews = props => {
+const styles = theme => ({
+	root: {
+		display: 'flex',
+		flexWrap: 'wrap',
+		justifyContent: 'space-around',
+		overflow: 'hidden',
+		backgroundColor: theme.palette.background.paper,
+	},
+	gridList: {
+		width: 500,
+		height: 450,
+	},
+	icon: {
+		color: 'rgba(255, 255, 255, 0.54)',
+	},
+})
+
+function TitlebarGridList(props) {
+	const { classes } = props
 	const news = props.news
 
 	return (
-		<div className="container_news">
-			{news.map((story, index) => (
-				<NewsItem key={story.urlToImage} image={story.urlToImage} />
+		<div className={classes.root}>
+			{news.map(tile => (
+				<NewsItem news={tile} key={tile.urlToImage} />
 			))}
 		</div>
 	)
 }
 
-export default LatestNews
+TitlebarGridList.propTypes = {
+	classes: PropTypes.object.isRequired,
+}
+
+export default withStyles(styles)(TitlebarGridList)
