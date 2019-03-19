3ec63d13576c15b17bf2a29d2642b6edbb26ef0f
Mike Tallerico <mtallerico1@gmail.com>
2019-03-18 23:17:20 -0400

    adds functionality to handle saved article state

--- client/src/components/newsItem.js
+++ client/src/components/newsItem.js
@@ -10,7 +10,9 @@ import CardMedia from '@material-ui/core/CardMedia'
 import Typography from '@material-ui/core/Typography'
 import Fab from '@material-ui/core/Fab'
 import AddIcon from '@material-ui/icons/Add'
+import DeleteIcon from '@material-ui/icons/Delete'
 import { IconButton } from '@material-ui/core'
+const axios = require('axios')
 
 const styles = theme => ({
 	card: {
@@ -31,12 +33,76 @@ const styles = theme => ({
 class MediaCard extends Component {
 	constructor(props) {
 		super(props)
-		this.state = {}
+		this.state = {
+			urlToImage: '',
+			title: '',
+			url: '',
+			saved: false,
+			sessionID: '',
+			userID: '',
+		}
+	}
+
+	saveStory = () => {
+		if (this.state.sessionID && !this.state.saved) {
+			axios
+				.post('http://localhost:3001/api/saved_story', {
+					urlToImage: this.state.urlToImage,
+					title: this.state.title,
+					url: this.state.url,
+					sessionID: this.state.sessionID,
+					userID: this.state.userID,
+				})
+				.then(res => {
+					if (res.status === 204) {
+						console.log('true')
+						this.setState({
+							saved: true,
+						})
+					}
+				})
+		} else {
+			alert('Article already saved')
+		}
+	}
+
+	componentDidMount() {
+		this.setState({
+			urlToImage: this.props.news.urlToImage,
+			title: this.props.news.title,
+			url: this.props.news.url,
+			sessionID: this.props.sessionID,
+			userID: this.props.userID,
+		})
+		if (this.props.isSaved) {
+			this.setState({
+				saved: true,
+			})
+		}
 	}
 
 	render() {
 		const { classes } = this.props
 		const { news } = this.props
+		let isSaved
+		let icon = (
+			<Fab color="primary" aria-label="Add" className={classes.fab}>
+				<IconButton>
+					<AddIcon onClick={this.saveStory} />
+				</IconButton>
+			</Fab>
+		)
+
+		if (this.state.saved) {
+			icon = (
+				<Fab color="primary" aria-label="Add" className={classes.fab}>
+					<IconButton>
+						<DeleteIcon onClick={this.deleteStory} />
+					</IconButton>
+				</Fab>
+			)
+		}
+
 		return (
 			<Card className={classes.card}>
 				<CardActionArea>
@@ -53,11 +119,7 @@ class MediaCard extends Component {
 							Read
 						</Link>
 					</Typography>
-					<Fab color="primary" aria-label="Add" className={classes.fab}>
-						<IconButton>
-							<AddIcon />
-						</IconButton>
-					</Fab>
+					{icon}
 				</CardActions>
 			</Card>
 		)
