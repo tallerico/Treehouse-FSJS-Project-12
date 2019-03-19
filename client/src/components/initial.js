153f0804165e990052225b0f9c5119e65f2e7850
Mike Tallerico <mtallerico1@gmail.com>
2019-03-18 23:20:08 -0400

    removes unused imports

--- client/src/components/initial.js
+++ client/src/components/initial.js
@@ -1,6 +1,4 @@
 import React from 'react'
-import PropTypes from 'prop-types'
-import Grid from '@material-ui/core/Grid'
 import Typography from '@material-ui/core/Typography'
 
 function initialWelcome(props) {
